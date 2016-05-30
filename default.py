fec46cf133aeb77164213089ace9fbb4f13f9031
Andrew Phillips <skeledrew@gmail.com>
2016-05-30 17:06:42 -0500

    Anime Plus menus added

--- default.py
+++ default.py
@@ -24,6 +24,7 @@ def test_area():
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
     addon.add_directory({'folder': 'fldr3'}, {'title': 'Folder #3'})
     addon.add_directory({'folder': 'fldr4'}, {'title': str(len(addon.queries))})
+    addon.add_directory({'folder': 'fldr4'}, {'title': 'Did something go wrong?'})
 
     #xbmcplugin.endOfDirectory(addon_handle)
     addon.end_of_directory()
@@ -32,17 +33,51 @@ class main:
     def __init__(self):
         
         if len(addon.queries) == 1 and addon.queries['mode'] == 'main':
-            root_dir().get()
-           
-        else:
-            test_area()
+            # default query
+            dirs().root()
+        
+        try:
+            target_dir = addon.queries['dir']
             
-class root_dir:
+            if target_dir == 'lib':
+                dirs().lib()
+                
+            if target_dir == 'aplus':
+                dirs().aplus()
+        
+        except:
+            pass
+        
+        test_area()
+            
+class dirs:
     
-    def get(self):
+    def root(self):
         addon.add_directory({'dir': 'lib'}, {'title': 'Library'})
-        addon.add_directory({'dir': 'fav'}, {'title': 'Favorites'})
+        addon.add_directory({'dir': 'fav'}, {'title': 'My List'})
         addon.add_directory({'dir': 'cfg'}, {'title': 'Settings'})
+        addon.end_of_directory()
+        
+    def lib(self):
+        addon.add_directory({'dir': 'aplus'}, {'title': 'Anime Plus'})
+        addon.add_directory({'dir': 'adub'}, {'title': 'Anime Dub'})
+        addon.add_directory({'dir': 'acrazy'}, {'title': 'Anime Crazy'})
+        addon.add_directory({'dir': 'ablog'}, {'title': 'Anime Blog'})
+        addon.add_directory({'dir': 'afreak'}, {'title': 'Anime Freak'})
+        addon.add_directory({'dir': 'afav'}, {'title': 'Anime Fav'})
+        addon.add_directory({'dir': 'cdub'}, {'title': 'Cartoon Dub'})
+        addon.end_of_directory()
+        
+    def aplus(self):
+        addon.add_directory({'list': 'index'}, {'title': 'Index'})
+        addon.add_directory({'list': 'pop'}, {'title': 'Popular'})
+        addon.add_directory({'list': 'new'}, {'title': 'New'})
+        addon.add_directory({'list': 'recent'}, {'title': 'Recent'})
+        addon.add_directory({'list': 'ongoing'}, {'title': 'Ongoing'})
+        addon.add_directory({'list': 'complete'}, {'title': 'Completed'})
+        addon.add_directory({'list': 'genres'}, {'title': 'Genres'})
+        addon.add_directory({'list': 'recrel'}, {'title': 'Recent Releases'})
+        addon.add_directory({'list': 'recadd'}, {'title': 'Recently Added Series'})
         addon.end_of_directory()
         
 main()
